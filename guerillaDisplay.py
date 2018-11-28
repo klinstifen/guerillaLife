1b8752334a2791c31b02cb9d0f17943d189ec006
klinstifen <klinstifen@gmail.com>
2018-11-28 17:16:42 -0500

    rename and clean code

---
+++ guerillaDisplay.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
+import time
+
+class guerillaDisplay(object):
+    def __init__(self):
+        self.options = RGBMatrixOptions()
+        self.options.rows = 16
+        self.options.chain_length = 1
+        self.options.parallel = 1
+        self.options.hardware_mapping = 'regular'  # If you have an Adafruit HAT: 'adafruit-hat'
+        self.font = graphics.Font()
+        self.font.LoadFont("rpi-rgb-led-matrix/fonts/5x8.bdf")
+        self.textColor = graphics.Color(255, 0, 0)
+    def initiate(self):
+        self.matrix = RGBMatrix(options = self.options)
+        self.offscreen_canvas = self.matrix.CreateFrameCanvas()
+
+    def set(self, m):
+        self.msg = m
+
+    def cellOn(self,x,y,b):
+        pos = 3 #align left
+        #self.msg = "."
+        self.matrix.SetPixel(x,y,b,0,0)
+        #graphics.DrawText(self.offscreen_canvas, self.font, pos, 10, self.textColor, self.msg)
+        #self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
+
+    def cellOff(self,x,y):
+        self.matrix.SetPixel(x,y,0,0,0)
+
+    def clear(self):
+        self.matrix.Clear()
