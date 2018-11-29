0a723eb0c4a3cf1f750b4289ea9ebef8c4f0c1c4
klinstifen <klinstifen@gmail.com>
2018-11-29 17:54:00 -0500

    find color bug
    still hunting

--- guerillaDisplay.py
+++ guerillaDisplay.py
@@ -19,11 +19,10 @@ class guerillaDisplay(object):
         self.msg = m
 
     def cellOn(self,x,y,r,g,b):
-        pos = 3 #align left
-        #self.msg = "."
-        self.matrix.SetPixel(x,y,r,g,b)
-        #graphics.DrawText(self.offscreen_canvas, self.font, pos, 10, self.textColor, self.msg)
-        #self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
+        self.r = r
+        self.g = g
+        self.b = b
+        self.matrix.SetPixel(x,y,self.r,self.g,self.b)
 
     def cellOff(self,x,y):
         self.matrix.SetPixel(x,y,0,0,0)
